334b83652daa8be457d6ee6edf977cd9d9aa84a3
icecold120000 <qej.pham@gmail.com>
2022-06-17 17:16:31 +0200

    Récupération des scopes de l'utilsateur

--- src/components/Scope/ChoiceScope.tsx
+++ src/components/Scope/ChoiceScope.tsx
@@ -1,39 +1,65 @@
 import NavBar from "../Utils/Navbar";
-import {Button, Form, FormGroup} from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
 import {fetchJSONWithToken} from "../Utils/Api";
 import {useSelector} from "react-redux";
 
 export default function ChoiceScope() {
-    const [scopes, setScopes] = useState([])
+
+    const [scopes, setScopes] = useState<Array<IScope>>([])
+    const [roles, setRoles] = useState<Array<IRole>>([])
+    const [isDisabled, setIsDisabled] = useState(false);
+
     const uuid = useSelector((state: any) => state.uuid.uuid)
 
     useEffect(() => {
-        fetchJSONWithToken(`https://localhost:8000/api/user/${uuid}/scopes`)
-            .then(response => response.body)
-            .then(data => {
-                return setScopes(data);
+        fetchJSONWithToken(`https://localhost:8000/api/users/${uuid}`)
+            .then(response => response.body as Promise<IUser>)
+            .then((data: IUser) => {
+                data.scopes.map(
+                    scopeId => fetchJSONWithToken(
+                        `https://localhost:8000${scopeId}`)
+                        .then(response => response.body as Promise<IScope>)
+                        .then((data: IScope) => {
+                            if (scopes.includes(data) === false) {
+                                scopes.push(data)
+                                setScopes(scopes)
+                            }
+                        })
+                )
             })
-
     }, [uuid])
 
+    const handleClick = () => {
+        setIsDisabled(!isDisabled)
+    };
+
+    useEffect(() => {
+        scopes.map(
+            scope => {
+                fetchJSONWithToken(
+                    `https://localhost:8000${scope.role}`)
+                    .then(response => response.body as Promise<IRole>)
+                    .then((data: IRole) => {
+                        if (roles.includes(data)) {
+                            roles.push(data)
+                            setRoles(roles)
+                        }
+                    })
+            }
+        )
+    }, [uuid])
     return (
         <>
             <NavBar/>
             <h1>Choisir un scope</h1>
             <Form>
-                {scopes.length > 0 && scopes.map(scope =>
+                {scopes.length > 0 && scopes.map((scope, index) =>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label={scope?.role_id?.name}/>
+                        <Form.Check type="checkbox" value={scope.id} label={"Scope " + index} disabled={isDisabled}
+                                    onClick={handleClick}/>
                     </Form.Group>
                 )}
-
-                <FormGroup className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Scope 2"/>
-                </FormGroup>
-                <FormGroup className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Scope 3"/>
-                </FormGroup>
                 <Button className={"btn"} variant="success" type="submit">
                     Valider
                 </Button>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,54 @@
 /// <reference types="react-scripts" />
+interface IScope {
+    id: string,
+    user: string,
+    structure: string,
+    role: string,
+    isActive: boolean,
+    created_at: datetime,
+}
+
+interface IUser {
+    id: string,
+    email: string,
+    roles: [string],
+    password: string,
+    firstName: string,
+    lastName: string,
+    scopes: Array<IScope>,
+    userIdentifier: string,
+    events: Array<IEvent>,
+    genre: {
+        name: string,
+        value: string
+    }
+    fullName: string
+}
+
+interface IRole {
+    id: string,
+    name: string,
+    code: string,
+    feminineName: string | null,
+    ageSection: string,
+    icon: string | null
+}
+
+interface IEvent {
+    id: string,
+    title: string,
+    dateStart: string,
+    dateEnd: string,
+    description: string,
+    typeEvent: string,
+    active: true,
+    "concernedStructure": [
+        "string"
+    ],
+    "concernedRole": [
+        "string"
+    ],
+    "concernedUser": [
+        "string"
+    ]
+}
